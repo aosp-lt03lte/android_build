78cd5df14c4e6e97e4e6da5d5d12dd524b27e61c
davidmueller13 <david.mueller1013@gmail.com>
2016-12-14 00:26:26 +0100

    rename zip, Android 7.1.1

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -645,7 +645,7 @@ reboot_now("%(bcb_dev)s", "recovery");
 else if get_stage("%(bcb_dev)s") == "3/3" then
 """ % bcb_dev)
 
-  script.Print("The Android Open Source Project 7.1")
+  script.Print("The Android Open Source Project 7.1.1")
   script.Print(" ")
   script.AppendExtra("sleep (2);")
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
